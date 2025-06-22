93b84ce56bfcfc4870b23bcd048c6370d2cde11e
kunal <kashyap.kunal123@gmail.com>
2025-06-22 08:52:12 +0530

    practice working with mutliple state and payload with redux

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,12 +2,20 @@ import { useSelector, useDispatch } from 'react-redux';
 import classes from './Counter.module.css';
 
 const Counter = () => {   
-  function fetchReduxStateData(state) {
+  function fetchReduxStateCounterData(state) {
     return state.counter;
   }
 
+   function fetchReduxStateVisibleCounter(state) {
+    return state.counterVisible;
+  }
+
   // fetchReduxStateData  funciton will be executed by react-redux since 
-  const counter = useSelector(fetchReduxStateData); 
+  const counter = useSelector(fetchReduxStateCounterData); 
+
+//we can use mutliple times the useSelector to find the chunk of required state from store
+const showCounter = useSelector(fetchReduxStateVisibleCounter);
+
 
   // useDispatch doesnt accepts any argument but it returns a function, which will dispatch 
   // a function against a redux store
@@ -21,15 +29,34 @@ const Counter = () => {
     dispatch({type : "DECREASEMENT"});
   }
 
+  //for this method we are tighlty coupled with the function , insted we can send the payload to identify
+  //the factor by which the counter can be increased
+  function handleIncreseButtonClickBy5() {
+    dispatch({type : "INCREASEBY5"});
+  }
+
+  function handleIncreseButtonClick() {
+    dispatch({type : "INCREASE", factor: 5}); //of course factor can also be dynamic and can be taken into consideration by using input box
+  }
+
+  function handleCounterToggleClickEvent() {
+    dispatch({type: "TOGGLE_COUNTER"})
+  }
+
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {showCounter && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={handleIncrementButtonClick}>Increment</button>
+        <button onClick={handleIncreseButtonClick}>Increment By 5</button>
         <button onClick={handleDecrementButtonClick}>decrement</button>
       </div>
+      {/* challenge is to show the counter only by clicking on the toggle button
+      Sol -  we can pass multiple state to redux reducer and fetch back to show the counter
+      */}
+       <button onClick={handleCounterToggleClickEvent}>Toggle Counter</button>
     </main>
   );
 };
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,35 @@
-import {createStore} from 'redux';
+import { createStore } from "redux";
 
 //create a reducer function and attach to store created below
-function counterReducer(state = { counter: 0 }, action) {
+function counterReducer(state = { counter: 0, counterVisible: true }, action) {
+  if (action.type === "TOGGLE_COUNTER") {
+    return {
+      counter: state.counter,
+      counterVisible: !state.counterVisible,
+    };
+  }
   if (action.type === "INCRESEMENT") {
-    return { counter: state.counter + 1 };
+    return { counter: state.counter + 1, counterVisible: state.counterVisible };
   }
 
   if (action.type === "DECREASEMENT") {
-    return { counter: state.counter - 1 };
+    return { counter: state.counter - 1, counterVisible: state.counterVisible };
   }
+
+  // Instead of writing  a method for each factor of increament , we need to send payload to
+  // reducer's action  identify them and increase the value by that factor
+  if (action.type === "INCREASEBY5") {
+    return { counter: state.counter + 5, counterVisible: state.counterVisible };
+  }
+
+  // accept the payload sent and only increase by that factor
+  if (action.type === "INCREASE") {
+    return {
+      counter: state.counter + action.factor,
+      counterVisible: state.counterVisible,
+    };
+  }
+
   return state;
 }
 
